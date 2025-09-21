f30faba640e5f3f87649ba7b6a3b42ef71f3a0db
LT <support@teran.tech>
2025-09-21 07:17:51 -0700

    Bank Account Version 1.4 - Object-oriented approach to managing bank accounts.

---
+++ Account.py
@@ -0,0 +1,43 @@
+# Account class
+
+class Account():
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = balance
+        self.password = password
+
+    def deposit(self, amountToDeposit, password):
+        if password != self.password:
+            print('Sorry, incorrect password.')
+            return None 
+        
+        if amountToDeposit < 0:
+            print('You cannot deposit a negative amount.')
+            return None
+        
+        self.balance = self.balance + amountToDeposit
+        return self.balance
+    
+    def withdraw(self, amountToWithdraw, password):
+        if password != self.password:
+            print('Sorry, incorrect password.')
+            return None
+        
+        if amountToWithdraw < 0:
+            print('You cannot withdraw a negative amount.')
+            return None
+        
+        self.balance = self.balance - amountToWithdraw
+        return self.balance 
+    
+    def getBalance(self, password):
+        if password != self.password:
+            print('Sorry, incorrect password.')
+            return None
+        return self.balance
+    
+    def show(self):
+        print('\tName:', self.name)
+        print('\tBalance:', self.balance)
+        print('\tPassword:', self.password)
+        print()
